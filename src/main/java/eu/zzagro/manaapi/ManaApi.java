f07acb42837b62fa877d9dc6eade8668bf310e60
zZagro <samuel.nahm1@gmail.com>
2022-02-25 15:59:21 +0100

    Mana Api 1.1 released!

--- src/main/java/eu/zzagro/manaapi/ManaApi.java
+++ src/main/java/eu/zzagro/manaapi/ManaApi.java
@@ -2,15 +2,23 @@ package eu.zzagro.manaapi;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public final class ManaApi extends JavaPlugin {
 
     private static ManaApi plugin;
+
+    //public File configFile;
+    //public FileConfiguration config;
 
     public static ArrayList<Player> manaRecoverList = new ArrayList<>();
     public static HashMap<Player, Integer> manaMap = new HashMap<>();
@@ -19,6 +27,7 @@ public final class ManaApi extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "ManaAPI by zZagro successfully enabled!");
+        //createCustomConfig();
     }
 
     @Override
@@ -34,12 +43,52 @@ public final class ManaApi extends JavaPlugin {
         maxManaMap.put(player, amount);
     }
 
+    public static void addMaxMana(Player player, int amount) {
+        int mana = (getMaxMana(player)) + amount;
+        if (mana >= 0) {
+            maxManaMap.put(player, mana);
+        } else {
+            maxManaMap.put(player, 0);
+        }
+    }
+
+    public static void removeMaxMana(Player player, int amount) {
+        int mana = (getMaxMana(player)) - amount;
+        if (mana >= 0) {
+            maxManaMap.put(player, mana);
+        } else {
+            maxManaMap.put(player, 0);
+        }
+    }
+
     public static int getMaxMana(Player player) {
         return maxManaMap.get(player);
     }
 
     public static void setCurrentMana(Player player, int amount) {
         manaMap.put(player, amount);
+    }
+
+    public static void addCurrentMana(Player player, int amount) {
+        int mana = (getCurrentMana(player)) + amount;
+        if (mana >= 0 && mana <= getMaxMana(player)) {
+            manaMap.put(player, mana);
+        } else if (mana > getMaxMana(player)){
+            manaMap.put(player, getMaxMana(player));
+        } else {
+            manaMap.put(player, 0);
+        }
+    }
+
+    public static void removeCurrentMana(Player player, int amount) {
+        int mana = (getCurrentMana(player)) - amount;
+        if (mana >= 0 && mana <= getMaxMana(player)) {
+            manaMap.put(player, mana);
+        } else if (mana > getMaxMana(player)){
+            manaMap.put(player, getMaxMana(player));
+        } else {
+            manaMap.put(player, 0);
+        }
     }
 
     public static int getCurrentMana(Player player) {
@@ -69,4 +118,23 @@ public final class ManaApi extends JavaPlugin {
             setCurrentMana(player, maxMana);
         }
     }
+
+    /*public FileConfiguration getCustomConfig() {
+        return this.config;
+    }
+
+    private void createCustomConfig() {
+        configFile = new File(getDataFolder(), "custom.yml");
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            saveResource("custom.yml", false);
+        }
+
+        config = new YamlConfiguration();
+        try {
+            config.load(configFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }*/
 }
